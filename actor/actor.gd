e479b7f9799e1202de871f5387b3425149a7b379
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2023-01-04 23:36:00 -0500

    add jump to player

--- actor/actor.gd
+++ actor/actor.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name Actor
 
 const SPEED = 0
-const JUMP_VELOCITY = -100.0
+const JUMP_VELOCITY = -20.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
--- actor/player/player.gd
+++ actor/player/player.gd
@@ -1,20 +1,20 @@
-extends Actor
+extends CharacterBody2D
 
 class_name Player
 
-var animated_sprite = $AnimatedSprite2D
-var foot_collider = $Area2D/CollisionShape2D
+@onready var animation_player = $Area2D2/AnimationPlayer
+@onready var foot_collider = $Area2D2/Area2D/CollisionShape2D
 
-func _ready():
-	animated_sprite.play("run")
+
 	
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
-		animated_sprite.play("jump")
-		foot_collider.disabled = false
-		velocity = Vector2(0, 0).lerp(Vector2(0,-100), .1)
+		print("jump")
+		animation_player.play("jump")
 #		velocity.y = JUMP_VELOCITY * gravity
-	print(velocity)
-
 	move_and_slide()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	animation_player.play("run")
