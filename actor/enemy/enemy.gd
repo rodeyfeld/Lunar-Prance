78afcae1dbb54c26b49c3dab3159c23938410ac2
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2023-01-05 10:45:30 -0500

    add enemy and path for enemy

---
+++ actor/enemy/enemy.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+class_name Enemy
+
+@export var path_to_follow:Path2D
--- level/base.gd
+++ level/base.gd
@@ -1,3 +1,20 @@
 extends Node2D
 
 class_name Base
+
+@onready var ground_path = $GroundPath
+var path_follow_2d = null
+const bat_scene = preload("res://actor/enemy/bat/bat.tscn")
+
+
+func _ready():
+	path_follow_2d = PathFollow2D.new()
+	ground_path.add_child(path_follow_2d)
+	var bat = bat_scene.instantiate()
+	path_follow_2d.add_child(bat)
+
+
+func _physics_process(delta):
+	print(path_follow_2d)
+	path_follow_2d.progress_ratio += .01
+
