cf7135e364bb2bf8e36789072088fc55b79d3d55
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2023-01-14 14:30:05 -0500

    rearrange patroller nodes

--- actor/enemy/patroller.gd
+++ actor/enemy/patroller.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var patrol_path = $Path2D/PathFollow2D
 
 func _physics_process(delta):
+	print(patrol_path.progress_ratio)
 	patrol_path.progress_ratio += .1 * delta
 	if patrol_path.progress_ratio == 1:
 		print("done")
