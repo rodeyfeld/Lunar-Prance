bf95eaa5290e9ca41da72e6564498087ecffc3ae
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2023-01-26 20:58:45 -0500

    work on moon

--- level/zone/base_zone.gd
+++ level/zone/base_zone.gd
@@ -3,3 +3,18 @@ extends Node2D
 
 
 
+@onready var moon = $MoonParallaxBackground/ParallaxLayer/Sprite2D
+var reached_apex = false
+
+func _physics_process(delta):
+	print(moon.global_position, moon.position)
+	if moon.position.y < -350:
+		reached_apex = true
+		
+	if reached_apex:
+		moon.position.y += .1 
+	else:
+		moon.position.y -= .1
+	
+#	moon.global_position.x -= .1
+	
