475a009a9a5632679c046e93cfc04f9e5cf91015
eric.d.rodefeld@gmail.com <eric.d.rodefeld@gmail.com>
2023-01-29 16:59:54 -0500

    new enemies. add new enemies to zone1

--- actor/enemy/enemy.gd
+++ actor/enemy/enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name Enemy
 
 @onready var animated_sprite = $AnimatedSprite2D
-
+@export var speed:float = 100
 @export var path_to_follow:Path2D
 var is_hit:bool = false
 
@@ -16,4 +16,4 @@ func _on_area_2d_area_entered(_area):
 
 
 func _on_detection_zone_area_entered(area):
-	velocity.x = -100
+	velocity.x = speed  * -1 
